3531dae6d9b496dd994315f4a5643c4adab61c84
Ryan Baker <ryan.and.baker@gmail.com>
2024-02-29 16:26:11 -0500

    Add scoring and moving circle position on click

--- static/js/script.js
+++ static/js/script.js
@@ -1,19 +1,44 @@
 window.addEventListener("DOMContentLoaded", (event) => {
+    //Starts the game
     document.getElementById('start').addEventListener('click', function(){
-        console.log("test");
+        //Removes the starting text and button
         document.getElementById('intro').style.display='none';
         document.getElementById('start').style.display='none';
 
+        //Countdown timer
         let timeAlloted = 30;
         document.getElementById('clock').innerHTML = timeAlloted;
-        //let score = 0;
         const timer = setInterval(function(){
             timeAlloted--;
             document.getElementById('clock').innerHTML = timeAlloted;
             if (timeAlloted == 0){
-                clearInterval();
+                clearInterval(timer);
+                document.getElementById('clock').remove();
+                document.getElementById('circle').remove();
             }
         }, 1000);
-        //const getRandom = (min, max) => Math.floor(Math.random()*(max-min+1)+min)
+
+        //Create circle after button
+        const initCircle = document.createElement('div');
+        initCircle.setAttribute("id", "circle")
+        document.getElementById("gameSpace").append(initCircle)
+
+        //Set Random generator
+        const getRandom = (min, max) => Math.floor(Math.random() * (max - min + 1) + min);
+        const circle = document.querySelector('#circle');
+
+        //Select a random starting position for the circle
+        circle.style.left = getRandom(0, 150 - 20) + 'rem';
+        circle.style.top = getRandom(0, 70 - 20) + 'rem';
+
+        //Every time the user clicks on the circle, increase the score and randomly position the circle
+        let score = 0;
+        document.getElementById('circle').addEventListener('click', function(){
+            score++;
+            console.log(score);
+            circle.style.left = getRandom(0, 150 - 20) + 'rem';
+            circle.style.top = getRandom(0, 70 - 20) + 'rem';
+        });
     });
+    
 });
