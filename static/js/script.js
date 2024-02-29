db048e8afb016804e3e91f773e538f4937a480c8
Ryan Baker <ryan.and.baker@gmail.com>
2024-02-29 15:22:30 -0500

    Add removal of starting elements after button press

--- static/js/script.js
+++ static/js/script.js
@@ -1,5 +1,9 @@
-setTimeout(()=>{
-    const text=document.getElementById('intro');
+window.addEventListener("DOMContentLoaded", (event) => {
+    document.getElementById('start').addEventListener('click', function(){
+        console.log("test");
+        document.getElementById('intro').style.display='none';
+        document.getElementById('start').style.display='none';
 
-    text.style.display='none';
-}, 3000);
+        const getRandom = (min, max) => Math.floor(Math.random()*(max-min+1)+min)
+    });
+});
