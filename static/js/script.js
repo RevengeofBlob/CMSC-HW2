deb1a40b88692fa5b787c8b993a381116b19598a
Ryan Baker <ryan.and.baker@gmail.com>
2024-02-29 15:39:11 -0500

    Add Timer

--- static/js/script.js
+++ static/js/script.js
@@ -4,6 +4,16 @@ window.addEventListener("DOMContentLoaded", (event) => {
         document.getElementById('intro').style.display='none';
         document.getElementById('start').style.display='none';
 
-        const getRandom = (min, max) => Math.floor(Math.random()*(max-min+1)+min)
+        let timeAlloted = 30;
+        document.getElementById('clock').innerHTML = timeAlloted;
+        //let score = 0;
+        const timer = setInterval(function(){
+            timeAlloted--;
+            document.getElementById('clock').innerHTML = timeAlloted;
+            if (timeAlloted == 0){
+                clearInterval();
+            }
+        }, 1000);
+        //const getRandom = (min, max) => Math.floor(Math.random()*(max-min+1)+min)
     });
 });
