f6ddffd748bbcdafa586bed871d09e3908a202cf
Ryan Baker <ryan.and.baker@gmail.com>
2024-02-29 20:14:19 -0500

    Add name and score submission

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import sqlite3
 from flask import Flask, render_template, request
 
 app = Flask(__name__)
@@ -5,6 +6,9 @@ app = Flask(__name__)
 @app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
-        pass
+        connection = sqlite3.connect('user_info.db')
+        cursor = connection.cursor()
+        name = request.form['name']
+        points = request.form['jsvar']
 
     return render_template('index.html')
--- static/js/script.js
+++ static/js/script.js
@@ -6,15 +6,16 @@ window.addEventListener("DOMContentLoaded", (event) => {
         document.getElementById('start').style.display='none';
 
         //Countdown timer
-        let timeAlloted = 30;
+        let timeAlloted = 5;
         document.getElementById('clock').innerHTML = timeAlloted;
         const timer = setInterval(function(){
             timeAlloted--;
             document.getElementById('clock').innerHTML = timeAlloted;
             if (timeAlloted == 0){
                 clearInterval(timer);
-                document.getElementById('clock').remove();
+                document.getElementById('clock').innerHTML = 'Score: ' + score;
                 document.getElementById('circle').remove();
+                logScore(score);
             }
         }, 1000);
 
@@ -35,10 +36,39 @@ window.addEventListener("DOMContentLoaded", (event) => {
         let score = 0;
         document.getElementById('circle').addEventListener('click', function(){
             score++;
-            console.log(score);
             circle.style.left = getRandom(0, 150 - 20) + 'rem';
             circle.style.top = getRandom(0, 70 - 20) + 'rem';
         });
     });
     
+    //Prompt user to input their name for their score
+    function logScore(score){
+        document.getElementById('gameSpace').remove();
+        const centered = document.createElement('center');
+        const sendInfo = document.createElement('form');
+        sendInfo.setAttribute('method', 'POST');
+        
+        //Name input box
+        const insertInfo = document.createElement('input');
+        insertInfo.setAttribute('type', 'name');
+        insertInfo.setAttribute('name', 'name');
+        insertInfo.setAttribute('placeholder', 'Enter Name');
+        //Button to submit
+        const submission = document.createElement('input');
+        submission.setAttribute('type', 'submit');
+        submission.setAttribute('name', 'sumbit');
+        //Store the score
+        const points = document.createElement('input');
+        points.setAttribute('type', 'hidden');
+        points.setAttribute('name', 'jsvar');
+        points.setAttribute('value', score);
+
+        const br = document.createElement('br')
+        sendInfo.append(insertInfo);
+        sendInfo.append(br);
+        sendInfo.append(submission);
+        sendInfo.append(points);
+        centered.append(sendInfo);
+        document.body.append(centered);
+    }
 });
